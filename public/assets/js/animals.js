cd9694485555203210e1730cc1f319ceaa9080df
Stephen Carlin <scarlinj@gmail.com>
2022-03-19 21:22:39 -0700

    fixed animal search functionality

--- public/assets/js/animals.js
+++ public/assets/js/animals.js
@@ -4,7 +4,13 @@ const $displayArea = document.querySelector('#display-area');
 const printResults = resultArr => {
   console.log(resultArr);
 
-  const animalHTML = resultArr.map(({ id, name, personalityTraits, species, diet }) => {
+  const animalHTML = resultArr.map(({
+    id,
+    name,
+    personalityTraits,
+    species,
+    diet
+  }) => {
     return `
   <div class="col-12 col-md-5 mb-3">
     <div class="card p-3" data-id=${id}>
@@ -30,6 +36,18 @@ const getAnimals = (formData = {}) => {
   });
 
   console.log(queryUrl);
+
+  fetch(queryUrl)
+    .then(response => {
+      if (!response.ok) {
+        return alert('Error: ' + response.statusText);
+      }
+      return response.json();
+    })
+    .then(animalData => {
+      console.log(animalData);
+      printResults(animalData);
+    });
 
 };
 
@@ -57,7 +75,10 @@ const handleGetAnimalsSubmit = event => {
 
   const personalityTraits = personalityTraitArr.join(',');
 
-  const animalObject = { diet, personalityTraits };
+  const animalObject = {
+    diet,
+    personalityTraits
+  };
 
   getAnimals(animalObject);
 };
